e82a81be7ad71bd928cd29c8ab2a2a9cfc2e3990
yfsoftcom <yfsoftcom@126.com>
2019-03-10 09:57:31 +0800

    todo get the para

--- source/app.js
+++ source/app.js
@@ -5,28 +5,35 @@ const fpm = new Fpm({ disableBodyParser: '/notify'});
 const router = fpm.createRouter();
 
 router.post('/notify', async (ctx) =>{
-  let postData = ctx
-  // console.log(postData);
 
-  const { req } = ctx;
-
-  //创建空字符叠加数据片段
-  let data = '';
-
-  //2.注册data事件接收数据（每当收到一段表单提交的数据，该方法会执行一次）
-  req.on('data',  (chunk) => {
-    // chunk 默认是一个二进制数据，和 data 拼接会自动 toString
-    data += chunk;
-  });
-
-  // 3.当接收表单提交的数据完毕之后，就可以进一步处理了
-  //注册end事件，所有数据接收完成会执行一次该方法
-  req.on('end',  () => { 
+  try {
+    const data = parseJson(ctx);
     console.log(data);
     ctx.body = { code: 0 }
-  })
-  ctx.body = { code: 0 }
+  } catch (error) {
+    ctx.throw(error, 502);
+  }
+  
 });
+
+const parseJson = (ctx) => {
+  return new Promise( (rs, rj) => {
+    const { req } = ctx;
+
+    let data = '';
+
+    req.on('data',  (chunk) => {
+      data += chunk;
+    });
+    req.on('end',  () => { 
+      try {
+        rs(JSON.parse(data));
+      } catch (error) {
+        rj('not json type')
+      }
+    })
+  })
+}
 
 
 fpm.bindRouter(router)
