f254684b850c96b1fcac4f51adb2890a4d90b147
yfsoftcom <yfsoftcom@126.com>
2019-03-10 12:41:40 +0800

    todo get the para

--- source/app.js
+++ source/app.js
@@ -1,42 +1,10 @@
 const { Fpm } = require('yf-fpm-server');
 
-const fpm = new Fpm({ disableBodyParser: '/aa'});
+const fpm = new Fpm();
 
-const router = fpm.createRouter();
-
-router.post('/notify', async (ctx) =>{
-
-  try {
-    const data = ctx.request.body;
-    console.log('bodypaser', data);
-    ctx.body = { code: 0 }
-  } catch (error) {
-    ctx.throw(error, 502);
-  }
-  
-});
-
-const parseJson = (ctx) => {
-  return new Promise( (rs, rj) => {
-    const { req } = ctx;
-
-    let data = '';
-
-    req.on('data',  (chunk) => {
-      data += chunk;
-    });
-    req.on('end',  () => { 
-      try {
-        rs(JSON.parse(data));
-      } catch (error) {
-        rj('not json type')
-      }
-    })
-  })
-}
-
-
-fpm.bindRouter(router)
+fpm.substribe(`#webhook/tianyi/notify`, (topic, message) => {
+  console.log(topic, message);
+})
 
 fpm.run().then( () => {
     console.log('Ready to go...')
